cf1cfcba714b3049420cec87ab64fbc54f5959cb
Seiichiro Ito <terryito0819@gmail.com>
2021-09-01 16:29:22 +0900

    Initialize repo for firebase

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  semi: false,
-  tabWidth: 4,
+  // semi: false,
+  // tabWidth: 4,
   useTabs: false,
   printWidth: 80,
   endOfLine: "auto",
-  singleQuote: true,
+  // singleQuote: true,
   trailingComma: "es5",
   bracketSpacing: true,
   arrowParens: "always",
-};
+}

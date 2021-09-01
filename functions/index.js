04a6190a1752a2efff46f5abeb01d174447318f2
Seiichiro Ito <terryito0819@gmail.com>
2021-09-01 18:20:35 +0900

    Add user create function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!");
-  response.send("Hello from Firebase!");
+exports.newUserSignUp = functions.auth.user().onCreate((user) => {
+  return admin.firestore().collection("users").doc(user.uid).set({
+    displayName: user.displayName,
+    email: user.email,
+    photoUrl: user.photoURL,
+    bio: "",
+  });
 });

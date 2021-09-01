43f382446b5d353cf30b9d258a8286fc1dcfa8f4
Seiichiro Ito <terryito0819@gmail.com>
2021-09-01 16:15:24 +0900

    Initialize vite project

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-X6q-DVJetBpqTLm1mCPPeK-uAR4c5Gg",
+  authDomain: "novelog-275fb.firebaseapp.com",
+  projectId: "novelog-275fb",
+  storageBucket: "novelog-275fb.appspot.com",
+  messagingSenderId: "574481485089",
+  appId: "1:574481485089:web:226e260749361749768260",
+  measurementId: "G-6K22HZZ61K",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1 class="text-green-300">Hello, Vite</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
